2c76c1a760cbe2fdbb5d3dd7a3021288e2c92806
phle0409 <phle.0409@gmail.com>
2025-11-14 02:28:45 -0800

    ui 2d maze for people walk around community

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ mod person;
 mod settings;
 mod simulation;
 
+use simulation::Simulation;
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    let result = eframe::run_native(
+        "Epidemic Simulation - SIR Model",
+        options,
+        Box::new(|_cc| Ok(Box::new(Simulation::new()))),
+    );
+
+    match result {
+        Ok(_) => println!("Success!"),
+        Err(e) => println!("Error: {}", e),
+    }
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -4,13 +4,13 @@ use rand::Rng;
 use crate::person::{Person, PersonState};
 use crate::settings::*;
 
-pub struct App {
+pub struct Simulation {
     pub community: Vec<Person>,
     pub total_time: f32,
     pub community_size: usize,
 }
 
-impl App {
+impl Simulation {
     pub fn new() -> Self {
         let community_size = 100;
         let community: Vec<Person> = (0..community_size).map(|_| Person::new()).collect();
@@ -29,6 +29,38 @@ impl App {
     }
 }
 
+impl eframe::App for Simulation {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.update_community();
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let (response, painter) = ui.allocate_painter(
+                egui::vec2(SIMULATION_AREA_SIZE + 80.0, SIMULATION_AREA_SIZE + 80.0),
+                egui::Sense::hover(),
+            );
+
+            let rect = response.rect;
+            let border_offset_x = rect.left() + 40.0;
+            let border_offset_y = rect.top() + 40.0;
+            ui.visuals_mut().panel_fill = egui::Color32::BLACK;
+            let rect = egui::Rect::from_min_size(
+                egui::pos2(border_offset_x, border_offset_y),
+                egui::vec2(SIMULATION_AREA_SIZE, SIMULATION_AREA_SIZE),
+            );
+            painter.rect_filled(rect, 0.0, egui::Color32::BLACK);
+            painter.rect_stroke(rect, 0.0, egui::Stroke::new(3.0, egui::Color32::WHITE));
+
+            for person in &self.community {
+                let particle_pos =
+                    egui::pos2(border_offset_x + person.x, border_offset_y + person.y);
+                painter.circle_filled(particle_pos, 4.0, egui::Color32::BLUE);
+            }
+        });
+
+        ctx.request_repaint();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,7 +69,7 @@ mod tests {
 
     #[test]
     fn test_create_new_app() {
-        let app = App::new();
+        let app = Simulation::new();
         assert_eq!(app.total_time, 0.0);
         assert_eq!(app.community_size, 100);
         assert_eq!(app.community.len(), 100);
@@ -50,7 +82,7 @@ mod tests {
     /// Check that position should change. If position does not change, the velocity must change due to a corner case.
     #[test]
     fn test_update_community_first_person() {
-        let mut app = App::new();
+        let mut app = Simulation::new();
         let initial_x = app.community[0].x;
         let initial_y = app.community[0].y;
         let initial_velocity_x = app.community[0].velocity_x;
