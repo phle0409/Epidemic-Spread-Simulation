297aa577972d1b17733a7ea034805757e87da3f7
phle0409 <phle.0409@gmail.com>
2025-11-13 16:34:35 -0800

    implement new method for App simulations

--- src/simulation.rs
+++ src/simulation.rs
@@ -2,17 +2,23 @@ use eframe::egui;
 use rand::Rng;
 
 use crate::person::{Person, PersonState};
+use crate::settings::*;
 
 pub struct App {
-    community: Vec<Person>,
-    width: f32,
-    height: f32,
+    pub community: Vec<Person>,
+    pub total_time: f32,
+    pub community_size: usize,
 }
-
 
 impl App {
     pub fn new() -> Self {
-        let mut community: Vec<Person> = (0..100)
-        .map(|| Person::)
+        let community_size = 100;
+        let community: Vec<Person> = (0..community_size).map(|_| Person::new()).collect();
+
+        Self {
+            community,
+            total_time: 0.0,
+            community_size,
+        }
     }
 }
